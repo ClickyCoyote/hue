ee7efd44b0b70d44d0c236c392a63789d86d2d74
Clicky Freewheel <clicky.freewheel@pm.me>
2020-07-21 20:46:32 -0500

    Fixed setup

--- setup.js
+++ setup.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 const fs = require('fs')
+const axios = require('axios').default
 
 module.exports = {
   baseuri: process.env.HUE_BASEURI,
@@ -7,7 +8,7 @@ module.exports = {
   speed: +process.env.SPEED,
 
   getUsername: async () => {
-    const response = await axios.post(`http://${baseuri}/api`, { devicetype: 'rainbow' })
+    const response = await axios.post(`http://${process.env.HUE_BASEURI}/api`, { devicetype: 'rainbow' })
     if (response.data[0].error) throw Error('Please press Hue Bridge link button')
     const username = response.data[0].success.username
     fs.appendFile('.env', `HUE_USERNAME=${username}`, (err) => {
